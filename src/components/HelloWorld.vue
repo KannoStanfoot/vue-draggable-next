b9b65112788075c50e49ff9e9263d6d2615d464a
KannoStanfoot <kanno@stanfoot.com>
2022-12-14 16:50:33 +0900

    ✨ ドラッガーを導入した。

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,124 +1,73 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
   </div>
+  <draggable
+    :list="myArray"
+    @start="drag = true"
+    @end="drag = false"
+    item-key="id"
+    v-bind="dragOptions"
+    handle=".handle"
+  >
+    <template #item="{ element, index }">
+      <div class="list-group-item">
+        <span class="handle"> mdi-drag-horizontal-variant </span>
+        <span class="text">{{ element.name + index }} </span>
+      </div>
+    </template>
+  </draggable>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, reactive } from "vue";
+import draggable from "vuedraggable";
 
 export default defineComponent({
   name: "HelloWorld",
   props: {
     msg: String,
   },
+  components: {
+    draggable,
+  },
+  setup() {
+    const myArray = reactive([
+      { name: "1だよ", id: 0 },
+      { name: "2だよ", id: 1 },
+      { name: "3だよ", id: 2 },
+      { name: "4だよ", id: 3 },
+      { name: "5だよ", id: 4 },
+      { name: "6だよ", id: 5 },
+    ]);
+
+    const drag = ref(false);
+    return { myArray, drag };
+  },
+  computed: {
+    dragOptions() {
+      return {
+        animation: 200,
+        group: "description",
+        disabled: false,
+        ghostClass: "ghost",
+      };
+    },
+  },
 });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.ghost {
+  opacity: 0.75;
+  background: #c8ebfb;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.list-group-item {
+  padding: 5px;
+  border-bottom: solid #000000 1px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.handle {
+  cursor: move;
 }
 </style>
